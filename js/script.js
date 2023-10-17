55bf1bde3953d56c86aca37835f2ee869bea933f
alexraduca01 <alex.raduca01@icloud.com>
2023-10-17 14:39:40 +0200

    added palindrome function

--- js/script.js
+++ js/script.js
@@ -10,5 +10,28 @@ Generiamo un numero random (sempre da 1 a 5) per il computer (ovviamente usando 
 Sommiamo i due numeri
 Stabiliamo se la somma dei due numeri è pari o dispari (ovviamente  usando una funzione :faccia_nerd:)
 Dichiariamo chi ha vinto.
-*/ 
+*/
 
+function palindrome() {
+    
+    const domanda = document.getElementById('testo').value;
+
+    for(let a = 0; a < domanda.length; a++){
+        
+        if(domanda[a] !== domanda[domanda.length - 1 - a]){
+            result.innerHTML = domanda + ' ' + 'non è una palindroma !!';
+            return false;
+        }
+        
+    }
+
+    result.innerHTML = domanda + ' ' + 'è una palindroma !!';
+    return true;
+}
+
+const btn = document.querySelector('button');
+const wrapper = document.getElementById('wrapper');
+let result = document.createElement('div');
+wrapper.append(result);
+
+btn.addEventListener('click', palindrome);
